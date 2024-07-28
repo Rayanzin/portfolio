db6222c37f2ed1319d6ec31475ab708d771cafce
Rayan <139246019+Rayanzin@users.noreply.github.com>
2024-07-27 21:40:16 -0300

    projeto portfólio finalizado
    proximas atualizações já estão sendo elaboradas

---
+++ js/index.js
@@ -0,0 +1,31 @@
+function html(){
+    descricaoinnerHTML = ''
+    descricao.innerHTML = `<h3 class="titulos-descricao html shadow">HTML</h3>
+    <p class="paragrafo-descricao">HTML (Linguagem de Marcação de HiperTexto) é o bloco de construção mais básico da web. Define o significado e a estrutura do conteúdo da web.</p>`
+}
+function css(){
+    descricaoinnerHTML = ''
+    descricao.innerHTML = `<h3 class="titulos-descricao css shadow" >CSS</h3>
+    <p class="paragrafo-descricao">É uma linguagem de estilos utilizada para descrever a apresentação de um documento escrito em HTML. O CSS descreve como elementos são mostrados na tela, no papel, na fala ou em outras mídias.</p>`
+}
+function js(){
+    descricaoinnerHTML = ''
+    descricao.innerHTML = `<h3 class="titulos-descricao js shadow" >Javascript</h3>
+    <p class="paragrafo-descricao">JavaScript (às vezes abreviado para JS) é uma linguagem leve, interpretada e baseada em objetos com funções de primeira classe, mais conhecida como a linguagem de script para páginas Web</p>`
+}
+function boots(){
+    descricaoinnerHTML = ''
+    descricao.innerHTML = `<h3 class="titulos-descricao bootstrap shadow" >Bootstrap</h3>
+    <p class="paragrafo-descricao">Bootstrap é um framework web com código-fonte aberto para desenvolvimento de componentes de interface e front-end para sites e aplicações web, usando HTML, CSS e JavaScript, baseado em modelos de design para a tipografia, melhorando a experiência do usuário em um site amigável e responsivo.</p>`
+}
+function github(){
+    descricaoinnerHTML = ''
+    descricao.innerHTML = `<h3 class="titulos-descricao github shadow" >Github</h3>
+    <p class="paragrafo-descricao">GitHub é uma plataforma de hospedagem de código-fonte e arquivos com controle de versão usando o Git. Ele permite que programadores, utilitários ou qualquer usuário cadastrado na plataforma contribuam em projetos privados e/ou Open Source de qualquer lugar do mundo.</p>`
+}
+function git(){
+    descricaoinnerHTML = ''
+    descricao.innerHTML = `<h3 class="titulos-descricao git shadow" >Git</h3>
+    <p class="paragrafo-descricao">Git é um sistema de controle de versões distribuído, usado principalmente no desenvolvimento de software, mas pode ser usado para registrar o histórico de edições de qualquer tipo de arquivo.</p>`
+}
+html()
